89a09f4a55d7e9bb0536504d2382e69da3b60cc2
Stanislav <shiptya@icloud.com>
2018-08-13 12:23:54 +0300

    ** Some customer xotelka

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,6 +23,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
+    {!! setting('site.additional') !!}
+
     <!-- Bootstrap Grid v4.1.0 -->
     <style>
         html {
@@ -2309,10 +2311,6 @@
             document.getElementById('burger').style.display = 'block';
         }
     </script>
-
-
-    {{ setting('site.additional') }}
-
 </head>
 
 <body>
